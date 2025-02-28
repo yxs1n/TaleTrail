f5ff35f23de507bbe0247c76889a91681836228d
Hamidou Wane <wanen717@gmail.com>
2025-02-28 11:17:04 +0000

    Created book table with columns

--- database.py
+++ database.py
@@ -1,3 +1,19 @@
 import sqlite3
 
 conn = sqlite3.connect('books.db')
+
+cursor = conn.cursor()
+
+#Create a table
+cursor.execute("""CREATE TABLE bookInfo (
+               id integer,
+               name text,
+               pages integer,
+               band text
+               )""")
+
+# Commit command
+conn.commit()
+
+# Close connection
+conn.close()

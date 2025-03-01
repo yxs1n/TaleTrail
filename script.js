bf9deb3bc10360ca2c5c3baa336390b679180f82
m-adebayo <149477242+m-adebayo@users.noreply.github.com>
2025-03-01 21:38:08 +0000

    Started Parent Interface, added "Add Log" Popup and the book entry. (still going to work on this)

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Log Popup */
+
+function openAddLog(){
+    document.getElementById("myPopup").style.display = "block";
+}
+
+function closeAddLog(){
+    document.getElementById("myPopup").style.display = "none";
+}
+
+/* Roadmap Popup */
+
+function openRoadmap(){
+    document.getElementById("roadmap-popup").style.display = "block";
+}
+
+function closeRoadmap(){
+    document.getElementById("roadmap-popup").style.display = "none";
+}

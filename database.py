76900616e42d6f683b5fd91f2d14873d7caa670a
Hamidou Wane <wanen717@gmail.com>
2025-03-03 11:42:05 +0000

    Added 5 records to table

--- database.py
+++ database.py
@@ -5,8 +5,7 @@ conn = sqlite3.connect('books.db')
 cursor = conn.cursor()
 
 #Create a table
-cursor.execute("""CREATE TABLE bookInfo (
-               id integer,
+cursor.execute("""CREATE TABLE IF NOT EXISTS bookInfo (
                title text,
                image blob,
                author text,
@@ -14,6 +13,9 @@ cursor.execute("""CREATE TABLE bookInfo (
                genre integer,
                band text
                )""")
+
+# Add single record
+cursor.execute("INSERT INTO bookInfo VALUES ('The Rainbow Fish', '', 'Marcus Pfister', 32, 'Juvenile Fiction', 3)")
 
 # Commit command
 conn.commit()
